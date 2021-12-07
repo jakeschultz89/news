9fc6e051ad9995f95182afeb20875eec5c73d14e
jakeschultz89 <ejacobschultz@gmail.com>
2021-12-07 12:38:46 -0500

    set up complete

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
+
+
+class SignUpView(CreateView):
+    template_name = 'registration/signup.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+
+class HomePageView(TemplateView):
+    template_name = 'index.html'
