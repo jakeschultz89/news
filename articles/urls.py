590486d756fcadc0a596833cb9e3748ea3475b4e
jakeschultz89 <ejacobschultz@gmail.com>
2021-12-11 11:56:47 -0500

    assignment 3

---
+++ articles/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import HomePageView, ArticleDetailView, ArticleListView, ArticleCreateView, ArticleUpdateView, ArticleDeleteView
+
+
+urlpatterns = [
+    path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('new/', ArticleCreateView.as_view(), name='article_new'),
+    path('<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
+]
